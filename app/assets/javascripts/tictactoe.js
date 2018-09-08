6aad24ed363a4569410ac224c2614a49f286e87a
Sarah Schaller <sarahcschaller@gmail.com>
2018-09-08 01:12:18 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,27 @@
-// Code your JavaScript / jQuery solution here
+$(function () {
+
+})
+
+const WIN_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+var turn = 0;
+var currentGame = 0;
+
+var player = () => turn % 2 ? 'O' : 'X';
+
+var updateState = (square) => $(square).text(player());
+
+var setMessage = (string) => $('#message').text(string);
+
+var checkWinner = () => {
+
+}
